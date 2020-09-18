bfe940322987ab5a0ea071d92d2476dd7986e461
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2020-09-18 23:18:42 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- problems/fibonacci_number/solution.java
+++ problems/fibonacci_number/solution.java
@@ -1,14 +1,10 @@
 class Solution {
-    public int fib(int N) {
-        int fibinachi = 0;
-        int fib2 = 1;
-        int fib3 = 0;
-        for (int i = 0; i < N; i++) {
-            fib3 = fibinachi + fib2;
-            fibinachi = fib2;
-            fib2 = fib3;
+    
+    public int fib(final int N) {
+        if (N == 0 || N == 1){
+            return N;
         }
-        return fibinachi;
+        return fib(N-1) + fib(N-2);
+        
     }
-
 }
