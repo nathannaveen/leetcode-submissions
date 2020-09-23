e9519a8422594159da9b0781f6f552925946975e
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2020-09-23 21:19:02 +0000

    Sync LeetCode submission - Rectangle Overlap (java)

--- problems/rectangle_overlap/solution.java
+++ problems/rectangle_overlap/solution.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        if (rec1[0]>=rec2[2]) {
-            return false;
-        }
-        if (rec2[0]>=rec1[2]) {
-            return false;
-        }
-        if (rec1[1]>=rec2[3]){ 
-            return false;
-        }
-        if (rec2[1]>=rec1[3]) {
-            return false;
-        }
-        else{
-            return true;
-        }
+        int t = rec1[0]; // I call it TACO, and TACO
+        int t2 = rec2[2]; //                    ||||
+        int a = rec2[0]; //                     2222
+        int a2 = rec1[2];
+        int c = rec1[1];
+        int c2 = rec2[3];
+        int o = rec2[1];
+        int o2 = rec1[3];
+        return t < t2 && a < a2 && c < c2 && o < o2;
+        
+
     }
 }
