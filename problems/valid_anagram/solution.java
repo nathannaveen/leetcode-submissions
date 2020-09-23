225947cf0568e95cfd5fe99a098a1757b55abd8f
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2020-09-23 17:15:08 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- problems/valid_anagram/solution.java
+++ problems/valid_anagram/solution.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.equals("") || t.equals("")){
-            return true;
-        }
-        int[] list = new int[26];
-        String[] first = s.split("");
-        String[] second = t.split("");
-        if (first.length != second.length){
+        if (s.length() != t.length()){
             return false;
         }
-
+        char[] first = s.toCharArray();
+        char[] second = t.toCharArray();
+        int[] letters = new int[26];
         for (int i = 0; i < first.length; i++) {
-            list[(int) first[i].charAt(0) - 'a'] += 1;
+            letters[first[i] - 'a'] += 1;
+            letters[second[i] - 'a'] -= 1;
         }
-
-        for (int i = 0; i < second.length; i++) {
-            list[(int) second[i].charAt(0) - 'a'] -= 1;
-        }
-  
-        for (int i = 0; i < list.length; i++) {
-            
-            if (list[i] != 0){
+        for (int i : letters) {
+            if (i != 0){
                 return false;
             }
         }
-
         return true;
-        
-        // System.out.println( (int) "b".charAt(0) - 'a' );
     }
 }
