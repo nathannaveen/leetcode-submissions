e31cafd837092ef4e7d6a5be66e47c121350ea7f
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2020-09-24 12:11:34 +0000

    Sync LeetCode submission - Reverse Integer (java)

--- problems/reverse_integer/solution.java
+++ problems/reverse_integer/solution.java
@@ -1,20 +1,19 @@
+
 class Solution {
     public int reverse(int x) {
-        long Result = 0;
+        long result = 0;
 
-        while (x != 0) {
-            Result = Result * 10;
-            Result += x % 10;
-            x = x / 10;
+        while (x != 0){
+            result *= 10;
+            result += x%10;
+            x /= 10;
         }
 
-        System.out.println(Result);
-        if (Result < Integer.MIN_VALUE || Result > Integer.MAX_VALUE) {
+        System.out.println(result);
+        if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
             return 0;
-        }
-
-        else {
-            return (int) Result;
+        } else {
+            return (int) result;
         }
 
     }
