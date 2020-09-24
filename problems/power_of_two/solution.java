bf84330536d17b98ccaaf0e99e2e7bf439276e30
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2020-09-24 18:09:16 +0000

    Sync LeetCode submission - Power of Two (java)

--- problems/power_of_two/solution.java
+++ problems/power_of_two/solution.java
@@ -1,19 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n == 1){
-            return true;
-        }
-        if (n == 0 || n < 0){
-            return false;
-        }
-        int newN = n;
-        while (newN > 1){
-            if (newN %2  == 1){
-                return false;
-            }
-            newN /= 2;
-            
-        }
-        return true;
+        return n > 0 && (n & (n-1)) == 0;
     }
 }
