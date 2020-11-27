d83627639d1af1fc18c401a41e2cf149e80a3bf4
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2020-11-27 18:13:30 +0000

    Sync LeetCode submission - Array Partition I (java)

---
+++ problems/array_partition_i/solution.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += Math.min(nums[i], nums[i+1]);
+        }
+        return sum;
+    }
+}
