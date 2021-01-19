35a59ba7068123b68a3cb0d0d819242885319108
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-01-19 20:12:47 +0000

    Sync LeetCode submission - Convert BST to Greater Tree (java)

---
+++ problems/convert_bst_to_greater_tree/solution.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        List<Integer> total = new ArrayList<>();
+        int totalSum = 0;
+        Stack<TreeNode> h = new Stack<>();
+//        Stack<TreeNode> j = new Stack<>();
+//        j.add(root);
+        h.add(root);
+
+        while (!h.isEmpty()){
+            TreeNode g = h.pop();
+            if (g != null){
+                total.add(g.val);
+                totalSum += g.val;
+                h.push(g.right);
+                h.push(g.left);
+            }
+        }
+
+        Collections.sort(total);
+        h.push(root);
+
+        while (!h.isEmpty()){
+            TreeNode g = h.pop();
+            if (g != null){
+                int sum = 0;
+                for (int i = 0; i < total.size(); i++) {
+                    if (total.get(i) == g.val) break;
+                    else sum += total.get(i);
+                }
+                g.val = totalSum - sum;
+                h.push(g.right);
+                h.push(g.left);
+            }
+        }
+
+        return root;
+    }
+}
