d7ecce83313f50a17a629da683ca788c5dcb5747
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-01-22 15:07:52 +0000

    Sync LeetCode submission - Number of Segments in a String (java)

---
+++ problems/number_of_segments_in_a_string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countSegments(String s) {
+        int h = 0;
+        for (int i = 0; i < s.length(); i++)
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' '))
+                h++;
+        return h;
+    }
+}
