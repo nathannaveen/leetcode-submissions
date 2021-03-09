a9a18f76c969021185ae20fbec5eaac516da9077
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-03-09 15:27:04 +0000

    Sync LeetCode submission - Check if Array Is Sorted and Rotated (golang)

--- problems/check_if_array_is_sorted_and_rotated/solution.go
+++ problems/check_if_array_is_sorted_and_rotated/solution.go
@@ -1,24 +1,15 @@
 func check(nums []int) bool {
-	
-	h := []int{}
-
-	for _, num := range nums {
-		h = append(h, num)
-	}
-
-	sort.Ints(h)
+	counter := 0
 
 	for i := 0; i < len(nums); i++ {
-		if reflect.DeepEqual(h, nums) {
-			return true
+		if nums[i] > nums[(i+1)%len(nums)] {
+			counter++
+			if counter >= 2 {
+				return false
+			}
 		}
 		
-		n := nums[0]
-
-		for j := 1; j < len(nums); j++ {
-			nums[j - 1] = nums[j]
-		}
-		nums[len(nums) - 1] = n
 	}
-	return false
+
+	return true
 }
