9243e15e03fa2fb645dfc08ae6150d427c4630a7
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-03-18 15:39:01 +0000

    Sync LeetCode submission - Sort Colors (golang)

--- problems/sort_colors/solution.go
+++ problems/sort_colors/solution.go
@@ -1,26 +1,8 @@
 func sortColors(nums []int) {
-	white := 0
-	blue := 0
-	counter := 0
-	
-	for _, num := range nums {
-		if num == 0 {
-			nums[counter] = 0
-			counter ++
-		} else if num == 1 {
-			white++
-		} else {
-			blue ++
+	for i := 1; i < len(nums); i++ {
+		if i >= 1 && nums[i-1] > nums[i] {
+			nums[i-1], nums[i] = nums[i], nums[i-1]
+			i -= 2
 		}
-	}
-
-	for i := 0; i < white; i++ {
-		nums[counter] = 1
-		counter ++
-	}
-    fmt.Println(nums)
-	for i := 0; i < blue; i++ {
-		nums[counter] = 2
-		counter ++
 	}
 }
