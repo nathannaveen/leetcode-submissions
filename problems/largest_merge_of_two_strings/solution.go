95c38c67cdea36949f4ef6c6164a5068c099bd04
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-05-24 12:49:10 +0000

    Sync LeetCode submission - Largest Merge Of Two Strings (golang)

--- problems/largest_merge_of_two_strings/solution.go
+++ problems/largest_merge_of_two_strings/solution.go
@@ -2,6 +2,7 @@ func largestMerge(word1 string, word2 string) string {
 	res := ""
 
 	for len(word1) != 0 && len(word2) != 0 {
+
 		if word1 > word2 {
 			res += string(word1[0])
 			word1 = word1[1:]
@@ -10,7 +11,6 @@ func largestMerge(word1 string, word2 string) string {
 			word2 = word2[1:]
 		}
 	}
-	res += word2
-	res += word1
+	res += word1 + word2
 	return res
 }
