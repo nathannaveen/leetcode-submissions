1ed0d7e146ee962bddc654435a7c25a651604582
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-06-05 13:31:15 +0000

    Sync LeetCode submission - Maximum Nesting Depth of Two Valid Parentheses Strings (golang)

--- problems/maximum_nesting_depth_of_two_valid_parentheses_strings/solution.go
+++ problems/maximum_nesting_depth_of_two_valid_parentheses_strings/solution.go
@@ -1,14 +1,15 @@
 func maxDepthAfterSplit(seq string) []int {
 	res := make([]int, len(seq))
-	pos := 0
+	evenOrOdd := 0
 	for i, i2 := range seq {
 		if i2 == '(' {
-			pos++
-			res[i] = pos % 2
+			evenOrOdd++
+			res[i] = evenOrOdd % 2
 		} else {
-			res[i] = pos % 2
-			pos--
+			res[i] = evenOrOdd % 2
+			evenOrOdd--
 		}
 	}
 	return res
 }
+
