27085a05f537afcfc3b4365af899088f331ff72c
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-06-12 13:55:01 +0000

    Sync LeetCode submission - Unique Email Addresses (golang)

--- problems/unique_email_addresses/solution.go
+++ problems/unique_email_addresses/solution.go
@@ -1,15 +1,16 @@
 func numUniqueEmails(emails []string) int {
 	m := make(map[string]int)
-	res := 0
 
 	for _, email := range emails {
 		end := strings.Index(email, "@")
 		email = strings.ReplaceAll(email[:end], ".", "") + email[end:]
 		start := strings.Index(email, "+")
 		end = strings.Index(email, "@")
-		if start != -1 { email = email[:start] + email[end:] }
-		if m[email] == 0 { res, m[email] = res + 1, 1 }
+		if start != -1 { 
+            email = email[:start] + email[end:] 
+        }
+		m[email]++
 	}
 
-	return res
+    return len(m)
 }
