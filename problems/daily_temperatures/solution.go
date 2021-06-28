bb0fa0c026cef930e901dbc9bf8d436cb15a7ce0
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-06-28 19:24:55 +0000

    Sync LeetCode submission - Daily Temperatures (golang)

--- problems/daily_temperatures/solution.go
+++ problems/daily_temperatures/solution.go
@@ -1,17 +1,29 @@
+type key struct {
+    val int
+    index int
+}
+
 func dailyTemperatures(T []int) []int {
-	h := []int{}
-	for i, i2 := range T {
-		containsGreater := false
-		for j := i; j < len(T); j++ {
-			if i2 < T[j] {
-				h = append(h, j - i)
-				containsGreater = true
-				break
-			}
-		}
-		if !containsGreater {
-			h = append(h, 0)
-		}
-	}
-	return h
+    stack := []key{}
+    res := make([]int, len(T))
+    
+    for i := len(T) - 1; i >= 0; i-- {
+        
+        counter := 0
+        
+        for j := len(stack) - 1; j >= 0 && T[i] >= stack[j].val; j-- {
+            counter++ // Add to counter so we don't keep on re-making the stack
+        }
+        
+        stack = stack[:len(stack) - counter]
+        res[i] = 0
+        
+        if len(stack) != 0 {
+            res[i] = stack[len(stack) - 1].index - i
+        }
+        
+        stack = append(stack, key{ T[i], i })
+    }
+    
+    return res
 }
