117372056922eb7ad3318a1b0c2fbe4161179476
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-07-03 04:09:27 +0000

    Sync LeetCode submission - Employee Free Time (golang)

---
+++ problems/employee_free_time/solution.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for an Interval.
+ * type Interval struct {
+ *     Start int
+ *     End   int
+ * }
+ */
+
+func employeeFreeTime(schedule [][]*Interval) []*Interval {
+    arr := []*Interval{}
+    res := []*Interval{}
+    
+    for _, ints := range schedule {
+        for _, i := range ints {
+            arr = append(arr, i)
+        }
+    }
+    
+    sort.Slice(arr, func(i, j int) bool {
+        return arr[i].Start <= arr[j].Start
+    })
+    
+    for i := 0; i < len(arr) - 1; i++ {
+        if arr[i].End < arr[i + 1].Start {
+            continue
+        }
+        if arr[i].End >= arr[i + 1].End {
+            arr = append(arr[:i + 1], arr[i+2:]...)
+        } else {
+            arr[i].End = arr[i + 1].End
+            arr = append(arr[:i + 1], arr[i+2:]...)
+        }
+        i--        
+    }
+    
+    for i := 0; i < len(arr) - 1; i++ {
+        res = append(res, &Interval{ arr[i].End, arr[i + 1].Start })
+    }
+    
+    return res
+}
