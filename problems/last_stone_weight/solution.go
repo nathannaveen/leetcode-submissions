866b7b73dbb8e325943241dbcb850e269864112a
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-08-01 01:47:03 +0000

    Sync LeetCode submission - Last Stone Weight (golang)

---
+++ problems/last_stone_weight/solution.go
@@ -0,0 +1,36 @@
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func lastStoneWeight(stones []int) int {
+    h := &IntHeap{}
+	heap.Init(h)
+    
+    for _, s := range stones {
+        heap.Push(h, s)
+    }
+    
+    for h.Len() > 1 {
+        heap.Push(h, heap.Pop(h).(int) - heap.Pop(h).(int))
+    }
+    
+    if h.Len() == 0 {
+        return 0
+    }
+    
+    return heap.Pop(h).(int)
+}
