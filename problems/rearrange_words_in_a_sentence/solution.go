f05f8bda6f584a51da53f0a4c965ef7c5add619f
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-08-07 15:56:17 +0000

    Sync LeetCode submission - Rearrange Words in a Sentence (golang)

--- problems/rearrange_words_in_a_sentence/solution.go
+++ problems/rearrange_words_in_a_sentence/solution.go
@@ -1,30 +1,11 @@
 func arrangeWords(text string) string {
-	split := strings.Split(text, " ")
-
-	for i := 1; i < len(split); i++ {
-		if len(split[i-1]) > len(split[i]) {
-			hasPut := false
-			l := split[i]
-			for j := i - 1; j >= 0; j-- {
-				if (len(split[j]) <= len(l)) || (j >= 1 && len(split[j-1]) <= len(l) && len(split[j]) >= len(l)) {
-					split[j+1] = split[j]
-					split[j] = l
-					hasPut = true
-					break
-				}
-				split[j+1] = split[j]
-			}
-
-			if !hasPut {
-				split[0] = l
-			}
-		}
-	}
-
-	g := strings.Join(split, " ")
-	g = strings.ToLower(g)
-	res := strings.ToUpper(string(g[0]))
-	res += g[1:]
-
-	return res
+    arr := strings.Split(text, " ")
+    
+    sort.SliceStable(arr, func(i, j int) bool {
+        return len(arr[i]) < len(arr[j])
+    })
+    
+    text = strings.ToLower(strings.Join(arr, " "))
+    temp := string(int(text[0]) - 32)
+    return temp + text[1:]
 }
