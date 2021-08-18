0bf033a565121c556b8c88ded0bf8f99fc332819
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-08-18 16:11:10 +0000

    Sync LeetCode submission - Distant Barcodes (golang)

--- problems/distant_barcodes/solution.go
+++ problems/distant_barcodes/solution.go
@@ -10,8 +10,6 @@ func (h KeyHeap) Less(i, j int) bool { return h[i].n > h[j].n }
 func (h KeyHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
 func (h *KeyHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
 	*h = append(*h, x.(key))
 }
 
