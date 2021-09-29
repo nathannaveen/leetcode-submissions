9eb44df505c74240a7461ecffe25317fcde034f9
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-09-29 12:14:27 +0000

    Sync LeetCode submission - Top K Frequent Words (golang)

--- problems/top_k_frequent_words/solution.go
+++ problems/top_k_frequent_words/solution.go
@@ -1,33 +1,55 @@
 func topKFrequent(words []string, k int) []string {
-	frequency := [][]int{}
+    frequency := [][]int{}
+    res := make([]string, k)
 
-	for i, word := range words {
-		contains := false
+    frequency = addWordsToFrequency(words, frequency)
 
-		for _, ints := range frequency {
-			if words[ints[0]] == word {
-				contains = true
-				ints[1]++
-				break
-			}
-		}
-		if !contains {
-			frequency = append(frequency, []int{i, 1})
-		}
-	}
+    sortFrequency(words, frequency)
 
-	for i := 0; i < len(frequency); i++ {
-		if i >= 1 && (frequency[i][1] < frequency[i-1][1] ||
-			(frequency[i][1] == frequency[i-1][1] && words[frequency[i][0]] > words[frequency[i-1][0]])) {
+    for i := len(frequency) - 1; i >= len(frequency)-k; i-- {
+        res[len(frequency) - i - 1] = words[frequency[i][0]]
+    }
+    return res
+}
 
-			frequency[i], frequency[i-1] = frequency[i-1], frequency[i]
-			i -= 2
-		}
-	}
-	res := []string{}
+func addWordsToFrequency(words []string, frequency [][]int) [][]int {
+    /*
+       adding all the words to the matrix called frequency
 
-	for i := len(frequency) - 1; i >= len(frequency) - k; i-- {
-		res = append(res, words[frequency[i][0]])
-	}
-	return res
+           * first check whether frequency has the word in it, if it does
+           add one to the frequency, if it doesn't add another word to the
+           array with a frequency of 1
+    */
+    for i, word := range words {
+        contains := false
+
+        for _, ints := range frequency {
+            if words[ints[0]] == word {
+                contains = true
+                ints[1]++
+                break
+            }
+        }
+        if !contains {
+            frequency = append(frequency, []int{i, 1})
+        }
+    }
+    return frequency
+}
+
+func sortFrequency(words []string, frequency [][]int) {
+    
+    /*
+    This fuction sorts the matrix called frequency.
+    */
+    
+    for i := 0; i < len(frequency); i++ {
+        if i >= 1 && (frequency[i][1] < frequency[i-1][1] ||
+            (frequency[i][1] == frequency[i-1][1] && 
+            words[frequency[i][0]] > words[frequency[i-1][0]])) {
+
+            frequency[i], frequency[i-1] = frequency[i-1], frequency[i]
+            i -= 2
+        }
+    }
 }
