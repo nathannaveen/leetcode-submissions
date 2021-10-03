78bee065d3774af097922ceabc7df5b27a02faab
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-10-03 13:43:45 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array (golang)

--- problems/find_minimum_in_rotated_sorted_array/solution.go
+++ problems/find_minimum_in_rotated_sorted_array/solution.go
@@ -1,20 +1,14 @@
 func findMin(nums []int) int {
-
-	for i := 1; i < len(nums)-1; i++ {
-		if nums[i] < nums[i-1] && nums[i] < nums[i+1] {
-			return nums[i]
-		}
-	}
-
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	if len(nums) == 2 {
-		return int(math.Min(float64(nums[0]), float64(nums[1])))
-	}
-	if nums[len(nums)-1] < nums[len(nums)-2] {
-		return nums[len(nums)-1]
-	}
-	return nums[0]
-	
+    left, right := 0, len(nums) - 1
+    
+    for left < right {
+        mid := (left + right) / 2
+        
+        if nums[mid] <= nums[right] {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return nums[left]
 }
