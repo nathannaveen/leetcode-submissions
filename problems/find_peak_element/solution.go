51cce1a1696959410d2ff2c0a6b570a8016b4817
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-10-03 13:44:02 +0000

    Sync LeetCode submission - Find Peak Element (golang)

--- problems/find_peak_element/solution.go
+++ problems/find_peak_element/solution.go
@@ -1,12 +1,15 @@
 func findPeakElement(nums []int) int {
-	for i := 1; i < len(nums)-1; i++ {
-		if nums[i] > nums[i-1] && nums[i] > nums[i+1] {
-			return i
-		}
-	}
-	
-	if len(nums) >= 2 && nums[len(nums)-1] > nums[len(nums)-2] {
-		return len(nums) - 1
-	}
-	return 0
+    left, right := 0, len(nums) - 1
+    
+    for left < right {
+        mid := (left + right) / 2
+        
+        if nums[mid + 1] > nums[mid] {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    
+    return left
 }
