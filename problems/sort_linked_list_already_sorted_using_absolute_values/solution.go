fe38bc6f4f5d9bf1b98af1a6d537dc7dcf60a592
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-11-01 13:36:12 +0000

    Sync LeetCode submission - Sort Linked List Already Sorted Using Absolute Values (golang)

--- problems/sort_linked_list_already_sorted_using_absolute_values/solution.go
+++ problems/sort_linked_list_already_sorted_using_absolute_values/solution.go
@@ -8,18 +8,18 @@
 func sortLinkedList(head *ListNode) *ListNode {
     cur := head.Next
     prev := head
-    start := head
     
     for cur != nil {
         if cur.Val < 0 {
-            start = &ListNode{ cur.Val, start }
-            cur = cur.Next
             prev.Next = prev.Next.Next
+            cur.Next = head
+            head = cur
+            cur = prev.Next
         } else {
             prev = prev.Next
             cur = cur.Next
         }
     }
     
-    return start
+    return head
 }
