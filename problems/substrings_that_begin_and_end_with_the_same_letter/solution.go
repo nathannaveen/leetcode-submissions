c23454b7b445307fd533526d3c604cffe044a277
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-11-27 16:19:50 +0000

    Sync LeetCode submission - Substrings That Begin and End With the Same Letter (golang)

--- problems/substrings_that_begin_and_end_with_the_same_letter/solution.go
+++ problems/substrings_that_begin_and_end_with_the_same_letter/solution.go
@@ -1,10 +1,13 @@
 func numberOfSubstrings(s string) int64 {
-    m := make(map[string] int)
+    m := make(map[rune] int)
     res := int64(0)
     
     for _, letter := range s {
-        m[string(letter)]++
-        res += int64(m[string(letter)])
+        m[letter]++
+    }
+    
+    for _, val := range m {
+        res += int64((val * (val + 1)) / 2)
     }
     
     return res
