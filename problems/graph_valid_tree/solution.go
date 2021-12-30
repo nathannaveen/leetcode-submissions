b3840f163efbc88de95b54dd522cb0329b3a3bcf
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-12-30 15:54:10 +0000

    Sync LeetCode submission - Graph Valid Tree (golang)

--- problems/graph_valid_tree/solution.go
+++ problems/graph_valid_tree/solution.go
@@ -1,34 +1,32 @@
 func validTree(n int, edges [][]int) bool {
-    counter := 0
-    
-    if len(edges) != n - 1 {
+    if len(edges) != n - 1 { // checking for a loop
         return false
     }
     
-    if len(edges) > 0 {
-        stack := []int{ edges[0][0] }
-        m := make(map[int] []int)
-        contains := make(map[int] int)
+    if n == 1 {
+        return true
+    }
+    
+    counter := 0
+    stack := []int{ edges[0][0] }
+    m := make(map[int] []int)
+    contains := make(map[int] int)
 
-        for _, i := range edges {
-            m[i[0]] = append(m[i[0]], i[1])
-            m[i[1]] = append(m[i[1]], i[0])
-        }
+    for _, i := range edges {
+        m[i[0]] = append(m[i[0]], i[1])
+        m[i[1]] = append(m[i[1]], i[0])
+    }
 
-        for len(stack) != 0 {
-            pop := stack[len(stack) - 1]
-            stack = stack[:len(stack) - 1]
+    for len(stack) != 0 {
+        pop := stack[len(stack) - 1]
+        stack = stack[:len(stack) - 1]
 
-            if contains[pop] != 1 {
-                counter++
-                stack = append(stack, m[pop]...)
-                contains[pop] = 1
-            }
-        }
-        
-        if counter != n {
-            return false
+        if contains[pop] != 1 {
+            counter++
+            stack = append(stack, m[pop]...)
+            contains[pop] = 1
         }
     }
-    return true
+
+    return counter == n
 }
