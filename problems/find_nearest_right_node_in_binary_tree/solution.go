c8669e9f0f02a775e14ad4269b53bf1f76a142f8
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-01-22 15:32:11 +0000

    Sync LeetCode submission - Find Nearest Right Node in Binary Tree (golang)

--- problems/find_nearest_right_node_in_binary_tree/solution.go
+++ problems/find_nearest_right_node_in_binary_tree/solution.go
@@ -7,28 +7,30 @@
  * }
  */
 func findNearestRightNode(root *TreeNode, u *TreeNode) *TreeNode {
-	queue := []*TreeNode{root}
-
-	for len(queue) != 0 {
-		n := len(queue)
-		for i := 0; i < n; i++ {
-			node := queue[0]
-			queue = queue[1:]
-			if node.Val == u.Val {
-				if i == n-1 {
-					return nil
-				} else {
-					return queue[0]
-				}
-			}
-			if node.Left != nil {
-				queue = append(queue, node.Left)
-			}
-			if node.Right != nil {
-				queue = append(queue, node.Right)
-			}
-		}
-	}
-
-	return nil
+    queue := []*TreeNode{root}
+    
+    for len(queue) != 0 {
+        n := len(queue)
+        
+        for i := 0; i < n; i++ {
+            pop := queue[0] // first element
+            queue = queue[1:] // actualy removing the first element
+            
+            if pop.Val == u.Val {
+                if i == n - 1 {
+                    return nil
+                }
+                return queue[0]
+            }
+            
+            if pop.Left != nil {
+                queue = append(queue, pop.Left)
+            }
+            if pop.Right != nil {
+                queue = append(queue, pop.Right)
+            }
+        }
+    }
+    
+    return nil
 }
