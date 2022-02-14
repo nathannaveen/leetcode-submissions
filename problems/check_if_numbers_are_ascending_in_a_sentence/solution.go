25284310346e20dfdd16acff176645668da6ae08
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-02-14 02:13:23 +0000

    Sync LeetCode submission - Check if Numbers Are Ascending in a Sentence (golang)

--- problems/check_if_numbers_are_ascending_in_a_sentence/solution.go
+++ problems/check_if_numbers_are_ascending_in_a_sentence/solution.go
@@ -1,11 +1,11 @@
 func areNumbersAscending(s string) bool {
     arr := strings.Split(s, " ")
-    prev := 0
+    prev := -1
     
     for _, a := range arr {
         n, _ := strconv.Atoi(a)
         
-        if n != 0 && prev < n { // check if it is a digit
+        if prev < n { // check if it is a digit
             prev = n
             continue
         } else if n != 0 { 
