c692c0fcb5c6571072d6c6a82ddc5fc8df073a75
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-03-02 17:01:42 +0000

    Sync LeetCode submission - Remove Duplicate Letters (golang)

---
+++ problems/remove_duplicate_letters/solution.go
@@ -0,0 +1,16 @@
+func removeDuplicateLetters(s string) string {
+    q := make([]byte, 0)
+    seen := make(map[byte]bool)
+    for i, _ := range s {
+        for len(q)>0 && q[len(q)-1]>s[i] && !seen[s[i]] && strings.Contains(s[i+1:], string(q[len(q)-1])) {
+            seen[q[len(q)-1]] = false
+            q = q[:len(q)-1]
+        }
+        if !seen[s[i]] {
+            q = append(q, s[i])
+            seen[s[i]] = true
+        }
+        
+    }
+    return string(q)
+}
