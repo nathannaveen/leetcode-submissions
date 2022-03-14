4cefee25118fc00cba738d80e8da1c36addb1403
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-03-14 16:07:10 +0000

    Sync LeetCode submission - K Closest Points to Origin (golang)

--- problems/k_closest_points_to_origin/solution.go
+++ problems/k_closest_points_to_origin/solution.go
@@ -1,17 +1,24 @@
-func kClosest(points [][]int, k int) [][]int {
-	for i := 1; i < len(points); i++ {
-		if i >= 1 {
-			distanceA := square(points[i - 1][0]) + square(points[i - 1][1])
-			distanceB := square(points[i][0]) + square(points[i][1])
-			if distanceA > distanceB {
-				points[i - 1], points[i] = points[i], points[i - 1]
-				i -= 2
-			}
-		}
-	}
-	return points[:k]
+type key struct {
+    x int
+    y int
+    dist float64
 }
 
-func square(n int) int {
-	return n * n
+func kClosest(points [][]int, k int) [][]int {
+    arr := []key{}
+    res := [][]int{}
+    
+    for _, point := range points {
+        x := point[0]
+        y := point[1]
+        
+        arr = append(arr, key{ x, y, math.Sqrt(math.Pow(float64(x), 2) + math.Pow(float64(y), 2)) })
+    }
+    sort.Slice(arr, func(i, j int) bool { return arr[i].dist < arr[j].dist })
+    
+    for i := 0; i < k; i++ {
+        res = append(res, []int{ arr[i].x, arr[i].y })
+    }
+    
+    return res
 }
