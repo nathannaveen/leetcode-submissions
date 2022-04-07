e2de81448313bb59f78295b05853643426a38cf9
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-04-07 14:25:30 +0000

    Sync LeetCode submission - Last Stone Weight (golang)

--- problems/last_stone_weight/solution.go
+++ problems/last_stone_weight/solution.go
@@ -1,21 +1,3 @@
-type IntHeap []int
-
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
 func lastStoneWeight(stones []int) int {
     h := &IntHeap{}
 	heap.Init(h)
@@ -33,4 +15,24 @@ func lastStoneWeight(stones []int) int {
     }
     
     return heap.Pop(h).(int)
+}
+
+// Heaps structure below:
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
