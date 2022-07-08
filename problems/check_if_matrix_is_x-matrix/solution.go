a1961981ad217972a754884470ffafc17844bf65
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-07-08 12:41:56 +0000

    Sync LeetCode submission - Check if Matrix Is X-Matrix (golang)

--- problems/check_if_matrix_is_x-matrix/solution.go
+++ problems/check_if_matrix_is_x-matrix/solution.go
@@ -1,11 +1,8 @@
 func checkXMatrix(grid [][]int) bool {
     for i := 0; i < len(grid); i++ {
         for j := 0; j < len(grid[0]); j++ {
-            if i + j == len(grid) - 1 || i == j {
-                if grid[i][j] == 0 {
-                    return false
-                }
-            } else if grid[i][j] != 0 {
+            if ((i + j == len(grid) - 1 || i == j) && grid[i][j] == 0) || 
+            (((i + j != len(grid) - 1 && i != j)) && grid[i][j] != 0) {
                 return false
             }
         }
