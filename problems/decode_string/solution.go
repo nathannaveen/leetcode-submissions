d637f5cf9aa57f1a84c7b668cbc973dbc77ad663
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2022-09-01 13:47:31 +0000

    Sync LeetCode submission - Decode String (golang)

--- problems/decode_string/solution.go
+++ problems/decode_string/solution.go
@@ -23,20 +23,16 @@ func helper(s string, res string) string {
         } else if s[i] < 97 && s[i + 1] < 97 && s[i + 2] < 97 && startCounter == 0 && 
         (s[i] != '[' && s[i] != ']' && s[i + 1] != '[' && s[i + 1] != ']' && s[i + 2] != '[' && s[i + 2] != ']')  {
             num = int(s[i] - '0') * 100 + int(s[i + 1] - '0') * 10 + int(s[i + 2] - '0')
-            // fmt.Println(num, "1")
             i += 2
         } else if s[i] < 97 && s[i + 1] < 97 && startCounter == 0 && 
         (s[i] != '[' && s[i] != ']' && s[i + 1] != '[' && s[i + 1] != ']') {
             num = int(s[i] - '0') * 10 + int(s[i + 1] - '0')
-            // fmt.Println(num, "2")
             i += 1
         } else if s[i] < 97 && startCounter == 0 &&
         (s[i] != '[' && s[i] != ']') {
             num = int(s[i] - '0')
-            // fmt.Println(num, "3")
         } else if startCounter == 0 {
             res += string(s[i])
-            // fmt.Println(string(s[i]), i)
         }
     }
     
