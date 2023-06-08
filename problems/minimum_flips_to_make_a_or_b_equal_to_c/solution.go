f160aeb49eb484d6e65e411fa3cf54d83041a0e6
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2023-06-08 14:40:40 +0000

    Sync LeetCode submission - Minimum Flips to Make a OR b Equal to c (golang)

--- problems/minimum_flips_to_make_a_or_b_equal_to_c/solution.go
+++ problems/minimum_flips_to_make_a_or_b_equal_to_c/solution.go
@@ -2,12 +2,7 @@ func minFlips(a int, b int, c int) int {
 	res := 0
 	for a > 0 || b > 0 || c > 0 {
 		if c & 1 == 0 {
-			if a & 1 == 1 {
-				res++
-			}
-			if b & 1 == 1 { 
-				res++ 
-			}
+			res += a & 1 + b & 1
 		} else if a & 1 == 0 && b & 1 == 0 {
 			res++
 		}
