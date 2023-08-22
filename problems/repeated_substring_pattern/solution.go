4a8fcd311b176a6589b5c3bfe3eb06f55086765c
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2023-08-22 16:15:49 +0000

    Sync LeetCode submission - Repeated Substring Pattern (golang)

--- problems/repeated_substring_pattern/solution.go
+++ problems/repeated_substring_pattern/solution.go
@@ -1,16 +1,5 @@
 func repeatedSubstringPattern(s string) bool {
-	for i := 0; i < len(s); i++ {
-		end := len(s)
-		str := ""
-		if i != 0 {
-			end = len(s) / i
-		}
-		for j := 0; j < end; j++ {
-			str += s[0:i]
-		}
-		if str == s {
-			return true
-		}
-	}
-	return false
+    x := s + s
+
+    return strings.Contains(x[1: len(x) - 1], s)
 }
