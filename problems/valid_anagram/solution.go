b93a175d7795939d495e4a2d89ca1ee9eda845c9
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2023-12-16 19:31:19 +0000

    Sync LeetCode submission - Valid Anagram (golang)

--- problems/valid_anagram/solution.go
+++ problems/valid_anagram/solution.go
@@ -1,19 +1,17 @@
 func isAnagram(s string, t string) bool {
-	if len(s) != len(t) {
-		return false
-	}
-	first := make([]int, 26)
-	second := make([]int, 26)
+    m := map[byte] int {}
 
-	for i := 0; i < len(s); i++ {
-		first[int(rune(s[i]) - 'a')] ++
-		second[int(rune(t[i]) - 'a')] ++
-	}
+    for i := 0; i < len(t); i++ {
+        m[t[i]]++
+    }
 
-	for i := 0; i < len(first); i++ {
-		if first[i] != second[i] {
-			return false
-		}
-	}
-	return true
+    for i := 0; i < len(s); i++ {
+        m[s[i]]--
+
+        if m[s[i]] == 0 {
+            delete(m, s[i])
+        }
+    }
+
+    return len(m) == 0
 }
