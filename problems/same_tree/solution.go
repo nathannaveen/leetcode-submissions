38e4f85ddbfa77096288ce851c8d63a2f52b1051
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2024-02-26 14:21:24 +0000

    Sync LeetCode submission - Same Tree (golang)

---
+++ problems/same_tree/solution.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p != nil && q != nil {
+        return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+    } else {
+        return p == q
+    }
+}
