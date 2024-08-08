0f51316818ca40cfb1ffb7b238eeec1e39421105
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2024-08-08 13:52:21 +0000

    Sync LeetCode submission Runtime - 6 ms (47.46%), Memory - 2.8 MB (100.00%)

---
+++ 3463-alternating-groups-i/solution.go
@@ -0,0 +1,34 @@
+func numberOfAlternatingGroups(colors []int) int {
+    full := false
+    res := 0
+    k := 3
+
+    for i := 0; i < len(colors); i++ {
+        if !full {
+            if helper(colors, k, i) {
+                full = true
+                res++
+            }
+        } else {
+            if colors[(i + k - 1) % len(colors)] != colors[(i + k - 2) % len(colors)] {
+                res++
+            } else {
+                full = false
+            }
+        }
+    }
+
+    return res
+}
+
+func helper(colors []int, k int, start int) bool {
+    prev := -1
+    for i := start; i < start + k; i++ {
+        if colors[i % len(colors)] == prev {
+            return false
+        }
+        prev = colors[i % len(colors)]
+    }
+
+    return true
+}
