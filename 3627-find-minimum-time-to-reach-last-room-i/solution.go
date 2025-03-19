c7dd57ca1491e32eb329ecad067ac6f12b771515
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2025-03-19 14:19:21 +0000

    Sync LeetCode submission Runtime - 26 ms (22.22%), Memory - 8.8 MB (33.33%)

---
+++ 3627-find-minimum-time-to-reach-last-room-i/solution.go
@@ -0,0 +1,57 @@
+import "container/heap"
+
+type key struct {
+    i, j int
+    t int
+}
+
+func minTimeToReach(moveTime [][]int) int {
+    h := &KeyHeap{}
+    heap.Init(h)
+    heap.Push(h, key{0, 0, 0}) // room (0, 0), at time 0
+
+    visited := map[key] bool {} // we are using the struct key as the key in the map to store the rooms i and j
+
+    for h.Len() > 0 {
+        pop := heap.Pop(h).(key)
+
+        if pop.i == len(moveTime) - 1 && pop.j == len(moveTime[0]) - 1 {
+            return pop.t
+        }
+
+        if visited[key{i : pop.i, j : pop.j}] {
+            continue
+        }
+        visited[key{i : pop.i, j : pop.j}] = true
+
+        for _, direction := range []key{{0, 1, 0}, {1, 0, 0}, {0, -1, 0}, {-1, 0, 0}} {
+            i, j := pop.i + direction.i, pop.j + direction.j
+
+            if inBounds(i, j, len(moveTime), len(moveTime[0])) {
+                t := max(moveTime[i][j], pop.t) + 1
+
+                heap.Push(h, key{i, j, t})
+            }
+        }
+    }
+
+    return -1
+}
+
+func inBounds(i, j, I, J int) bool {
+    return i >= 0 && j >= 0 && i < I && j < J
+}
+
+type KeyHeap []key
+
+func (h KeyHeap) Len() int           { return len(h) }
+func (h KeyHeap) Less(i, j int) bool { return h[i].t < h[j].t }
+func (h KeyHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *KeyHeap) Push(x any) { *h = append(*h, x.(key)) }
+func (h *KeyHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
